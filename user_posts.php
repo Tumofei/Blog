a5df3961ac6353d00a4bfe80fd6577deab4c3107
Timofei <tumka95@gmail.com>
2016-03-16 23:25:19 +0300

    add form and searching user

--- user_posts.php
+++ user_posts.php
@@ -1,15 +1,26 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
-  "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title> Посты пользователя</title>
  </head>
  <body>
- <?php
+ <form action="user_posts.php" method="post">
+     <fieldset>
+         <label for="name">Имя:</label><br/>
+         <input type="text" name="user_name" size="30"><br/>
+         <input type="submit" value="Найти пользователя"><br/>
+     </fieldset>
+     <br/>
+
+  <?php
  require ('connect.php');
 
- $res =  mysqli_query($link, "SELECT posts.name_post, posts.content, posts.date_create FROM posts JOIN users ON posts.id_users = users.id WHERE users.name = \"First_user\";");
+
+
+ $user_name=trim($_REQUEST['user_name']);
+
+ $res =  mysqli_query($link, "SELECT posts.name_post, posts.content, posts.date_create FROM posts JOIN users ON posts.id_users = users.id WHERE users.name = \"$user_name\";");
  if (!$res) {
     die('Invalid query: ' . mysqli_error($link));
 }
@@ -17,7 +28,7 @@
 ?>
  
     <table border='1'>
-            <thead> Список постов First user: </thead>
+            <thead> Список постов <?=$user_name?> : </thead>
             <tr>
                 <th>Название</th>
                 <th>Пост</th>
@@ -26,7 +37,7 @@
     <?php
     while($row = mysqli_fetch_array($res)): ?>
         <tr>
-            <td> <?=$row[0]?> </td>
+            <td><?=$row[0]?></td>
             <td> <?=$row[1]?> </td>
             <td> <?=$row[2]?> </td>
         </tr>
