59092c00c8b4a29899ac9709123c3aae16369853
Timofei <tumka95@gmail.com>
2016-03-16 23:24:14 +0300

    Add link user

--- user_list.php
+++ user_list.php
@@ -1,5 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
-  "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -11,7 +10,7 @@
  require ('connect.php');
 
 
- $res =  mysqli_query($link, "SELECT users.name, users.email,  COUNT(id) FROM posts LEFT OUTER JOIN users ON posts.id_users = users.id GROUP BY id;");
+ $res =  mysqli_query($link, "SELECT users.name, users.email,  COUNT(id_users) FROM posts RIGHT OUTER JOIN users ON users.id = posts.id_users GROUP BY name;");
  if (!$res) {
     die('Invalid query: ' . mysqli_error($link));
 }
@@ -28,7 +27,7 @@
 
             while($row = mysqli_fetch_array($res)): ?>
             <tr>
-                <td>  <?=$row[0]?> </td>
+                <td> <a href="/user_posts.php?user_name=<?=$row[0]?>"> <?=$row[0]?></a> </td>
                 <td>  <?=$row[1]?> </td>
                 <td> <?=$row[2]?> </td>
             </tr>
