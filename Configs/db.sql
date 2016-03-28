870c064652c92806c2ddd3b42c0aa499ff9d2589
Timofei <tumka95@gmail.com>
2016-03-28 11:16:59 +0200

    insert admin

--- Configs/db.sql
+++ Configs/db.sql
@@ -12,4 +12,4 @@ content TEXT,
 date_create DATE NOT NULL,
 id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
-
+INSERT INTO users (name, email, password, permission) VALUES ('admin' , 'admin@gmail.com' , '21232f297a57a5a743894a0e4a801fc3'  , 'admin' );
